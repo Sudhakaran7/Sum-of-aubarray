f809f688681524e16cb6e07de8e88c246fcc90c4
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-07-30 16:12:42 +0530

    Create sum-of-aubarray.py

---
+++ sum-of-aubarray.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def sumSubarrayMins(self, A):
+        MOD = 10**9 + 7
+        N = len(A)
+        stack = []
+        prev = [None] * N
+        for i in range(N):
+            while stack and A[i] <= A[stack[-1]]:
+                stack.pop()
+            prev[i] = stack[-1] if stack else -1
+            stack.append(i)
+        stack = []
+        next_ = [None] * N
+        for k in range(N-1, -1, -1):
+            while stack and A[k] < A[stack[-1]]:
+                stack.pop()
+            next_[k] = stack[-1] if stack else N
+            stack.append(k)
+        return sum((i - prev[i]) * (next_[i] - i) * A[i]
+                   for i in range(N)) % MOD
+val=Solution()
+a=list(map(int,input().split()))
+print(val.sumSubarrayMins(a))
